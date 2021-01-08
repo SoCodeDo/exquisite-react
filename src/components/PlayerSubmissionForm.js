bd98851dd8703a69ab200be952966f083566beb3
Sophia Donan <sophiadonan@gmail.com>
2021-01-08 05:49:52 -0800

    Still debugging tests for wave 1

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -5,14 +5,33 @@ import './PlayerSubmissionForm.css';
 
 const PlayerSubmissionForm = (props) => {
 
-  const values = {};
+  const emptyValues = {};
   props.fields.forEach(field => {
     if (field.key) {
-      values[field.key] = '';
+      emptyValues[field.key] = '';
     }
   });
 
-  const [formFields, setFormFields] = useState(values);
+  const [formFields, setFormFields] = useState(emptyValues);
+  const [player, setPlayer] = useState(1);
+
+  const onInputChange = (event) => {
+    const newFormFields = {
+      ...formFields,
+    }
+    newFormFields[event.target.name] = event.target.value;
+    setFormFields(newFormFields);
+  }
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    // props.addStudentCallback(formFields);
+    setPlayer(player + 1);
+    setFormFields(emptyValues);
+
+  };
+
 
   const inputComponents = props.fields.map((field, i) => {
     if (field.key) {
@@ -20,7 +39,8 @@ const PlayerSubmissionForm = (props) => {
         placeholder={field.placeholder}
         type="text"
         value={formFields[field.key]}
-        
+        name={field.key}
+        onChange={onInputChange}
       />;
     } else {
       return field;
@@ -29,16 +49,13 @@ const PlayerSubmissionForm = (props) => {
 
   return (
     <div className="PlayerSubmissionForm">
-      <h3>Player Submission Form for Player #{  }</h3>
+      <h3>Player Submission Form for Player #{player}</h3>
 
-      <form className="PlayerSubmissionForm__form" >
+      <form className="PlayerSubmissionForm__form" onSubmit={onFormSubmit}>
 
         <div className="PlayerSubmissionForm__poem-inputs">
 
           { inputComponents }
-          {/* <input
-            placeholder="hm..."
-          type="text" /> */}
 
         </div>
 
